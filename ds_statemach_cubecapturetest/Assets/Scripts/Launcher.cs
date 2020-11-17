861cbfd62a7a4766d48305197681f94adcd94705
Braden Fullmer <46699133+bradenfullmer@users.noreply.github.com>
2020-11-16 19:52:36 -0700

    3 hours of lonely work
    Did a bunch more shit

--- ds_statemach_cubecapturetest/Assets/Scripts/Launcher.cs
+++ ds_statemach_cubecapturetest/Assets/Scripts/Launcher.cs
@@ -5,102 +5,44 @@ using Photon.Realtime;
 using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks {
-    #region Private Serializable Fields
-    [Tooltip ("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
     [SerializeField]
-    private byte maxPlayersPerRoom = 4;
-
-    #endregion
-
-    #region Private Fields
-    [Tooltip ("The Ui Panel to let the user enter name, connect and play")]
+    private GameObject delayStartButton;
     [SerializeField]
-    private GameObject controlPanel;
-    [Tooltip ("The UI Label to inform the user that the connection is in progress")]
+    private GameObject delayCancelButton;
     [SerializeField]
-    private GameObject progressLabel;
+    private int roomSize;
 
-    bool isConnecting;
-
-    string gameVersion = "1";
-
-    #endregion
-
-    #region MonoBehaviour CallBacks
-
-    void Awake () {
+    public override void OnConnectedToMaster(){
         PhotonNetwork.AutomaticallySyncScene = true;
+        delayStartButton.SetActive(true);
     }
 
-    void Start () {
-        progressLabel.SetActive (false);
-        controlPanel.SetActive (true);
+    public void DelayStart(){
+        delayStartButton.SetActive(false);
+        delayCancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Delay Start");
     }
 
-    #endregion
-
-    #region Public Methods
-
-    public void Connect () {
-        progressLabel.SetActive (true);
-        controlPanel.SetActive (false);
-        if (PhotonNetwork.IsConnected) {
-            PhotonNetwork.JoinRandomRoom ();
-        } else {
-            isConnecting = PhotonNetwork.ConnectUsingSettings ();
-            PhotonNetwork.GameVersion = gameVersion;
-        }
+    public override void OnJoinRandomFailed(short returnCode, string message){
+        CreateRoom();
     }
 
-    #endregion
-
-    #region MonoBehaviourPunCallbacks Callbacks
-
-    public override void OnConnectedToMaster () {
-        Debug.Log ("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
-        if (isConnecting) {
-
-            PhotonNetwork.JoinRandomRoom ();
-            isConnecting = false;
-        }
+    void CreateRoom(){
+        Debug.Log("Creating room now");
+        int randomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize};
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+        Debug.Log(randomRoomNumber);
     }
 
-    public override void OnDisconnected (DisconnectCause cause) {
-        progressLabel.SetActive (false);
-        controlPanel.SetActive (true);
-        Debug.LogWarningFormat ("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    public override void OnCreateRoomFailed(short returnCode, string message){
+        Debug.Log("Failed to create room... trying again");
+        CreateRoom();
     }
-
-    public override void OnJoinRandomFailed (short returnCode, string message) {
-        Debug.Log ("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
-
-        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-        PhotonNetwork.CreateRoom (null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-    }
-
-    public override void OnJoinedRoom () {
-        Debug.Log ("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
-
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 1) {
-            Debug.Log ("We load the 'Room for 1' ");
-
-            PhotonNetwork.LoadLevel ("Stage 2");
-        }
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 2) {
-            Debug.Log ("We load the 'Room for 2' ");
-
-            PhotonNetwork.LoadLevel ("Stage 2");
-        }
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 3) {
-            Debug.Log ("We load the 'Room for 3' ");
-
-            PhotonNetwork.LoadLevel ("Stage 2");
-        }
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 4) {
-            Debug.Log ("We load the 'Room for 4' ");
-
-            PhotonNetwork.LoadLevel ("Stage 2");
-        }
+    public void DelayCancel(){
+        delayCancelButton.SetActive(false);
+        delayStartButton.SetActive(false);
+        PhotonNetwork.LeaveRoom();
     }
 }
-    #endregion
---
+++ ds_statemach_cubecapturetest/Assets/Scripts/PhotonRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonRoom : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int waitingRoomSceneIndex;
+
+    public override void OnEnable(){
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable(){
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom(){
+        SceneManager.LoadScene(waitingRoomSceneIndex);
+    }
+}
